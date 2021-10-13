75a836d25905c0c8b5b368143b1ee8d363f20fbc
ken <ken.mbira@student.moringaschool.com>
2021-10-13 19:39:03 +0300

    add user login features

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 def login_user(request):
     """This will handle the user registration
@@ -6,5 +10,18 @@ def login_user(request):
     Args:
         request ([type]): [description]
     """
-    message = 'Hello world!'
-    return render(request,'authenticate/login.html',{"message":message})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username,password = password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+
+        else:
+            messages.success(request,'You have entered invalid credentials, please check them and try again')
+            return render(request,'authenticate/login.html')
+
+    else:
+        return render(request,'authenticate/login.html')
