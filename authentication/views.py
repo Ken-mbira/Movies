8d63cf958f079fcbaf8bad97e637f5aeb4d512c2
ken <ken.mbira@student.moringaschool.com>
2021-10-13 20:13:24 +0300

    add registration functionality

--- authentication/views.py
+++ authentication/views.py
@@ -36,3 +36,26 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You were logged out successfully")
     return redirect('index')
+
+def register_user(request):
+    """This will manage the proccesses involved in registering a new user
+
+    Args:
+        request ([type]): [description]
+    """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,f"Congratulations {username}, you were successfully registered into Movie Galore")
+            return redirect('index')
+
+    else:
+        form = UserCreationForm()
+    return render(request,'authenticate/register_user.html',{'form':form})
+        
