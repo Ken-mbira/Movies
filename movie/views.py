e744587850714911b0294e02d7d3c0fd4b1c220d
ken <ken.mbira@student.moringaschool.com>
2021-10-13 18:21:29 +0300

    configured the first application

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """This will render the home page
+
+    Args:
+        request ([type]): [description]
+    """
+    return render(request,'movie/index.html')
