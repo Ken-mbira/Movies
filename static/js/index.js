77a10dcc63fa133a12dd403d9c40705e0a688b8d
ken <ken.mbira@student.moringaschool.com>
2021-10-14 09:31:00 +0300

    add bootstrap masonry

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+    $(window).on('load', function() {
+        setTimeout(function() {
+            var msnry = new Masonry('.grid');
+            msnry.layout();
+        }, 100);
+    });
 })
